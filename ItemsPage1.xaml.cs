f99e3ec18582e90291d7b1b622f297be4102bdd2
Hannah Kang <yeonjik@gmail.com>
2012-10-25 19:27:46 -0400

    all the files

---
+++ ItemsPage1.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Items Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234233
+
+namespace _4PLAN
+{
+    /// <summary>
+    /// A page that displays a collection of item previews.  In the Split Application this page
+    /// is used to display and select one of the available groups.
+    /// </summary>
+    public sealed partial class ItemsPage1 : _4PLAN.Common.LayoutAwarePage
+    {
+        public ItemsPage1()
+        {
+            this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// Populates the page with content passed during navigation.  Any saved state is also
+        /// provided when recreating a page from a prior session.
+        /// </summary>
+        /// <param name="navigationParameter">The parameter value passed to
+        /// <see cref="Frame.Navigate(Type, Object)"/> when this page was initially requested.
+        /// </param>
+        /// <param name="pageState">A dictionary of state preserved by this page during an earlier
+        /// session.  This will be null the first time a page is visited.</param>
+        protected override void LoadState(Object navigationParameter, Dictionary<String, Object> pageState)
+        {
+            // TODO: Assign a bindable collection of items to this.DefaultViewModel["Items"]
+        }
+
+        private void itemGridView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            string name = e.Parameter as string;
+
+            if (!string.IsNullOrWhiteSpace(name))
+            {
+                tb1.Text = "Hello, " + name;
+            }
+            else
+            {
+                tb1.Text = "Name is required.  Go back and enter a name.";
+            }
+            base.OnNavigatedTo(e);
+        }
+
+        private void Tasks_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.Frame != null)
+            {
+                this.Frame.Navigate(typeof(BasicPage1));
+
+            }
+        }
+
+    }
+}
